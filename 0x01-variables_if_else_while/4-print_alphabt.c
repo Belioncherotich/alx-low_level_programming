7ee2c850d0d71f299a03ba9206841540a6d896ba
Belioncherotich <velorinecodes@gmail.com>
2023-06-16 11:33:11 -0700

    Tasks on variables, if, else, while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,5 @@
-#include <string.h>
 #include <stdio.h>
+
 /**
  * main - Print alphabet with no q and e.
  *
@@ -10,10 +10,12 @@ int main(void)
 	int i;
 
 	for (i = 97; i < 123; i++)
-          {
-		  if (i != 101 && i != 113)
-                  putchar(i);
-          }
-          putchar('\n');
-return (0);
+	{
+		if (i != 101 && i != 113)
+			putchar(i);
+	}
+
+	putchar('\n');
+
+	return (0);
 }
