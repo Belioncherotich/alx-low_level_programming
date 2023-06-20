2722b169f65fdd572743bc5b4cc38afa077c0328
Belioncherotich <velorinecodes@gmail.com>
2023-06-20 14:26:47 -0700

    C - Functions, nested loops task

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
-  * main - Entry point
+  * print_alphabet_x10 - Prints lowercase alphabets 10 times
   * Describe: prints 10 times the alphabet in lowercase
   * followed by a new line.
   * Return: Always 0 (Success)
