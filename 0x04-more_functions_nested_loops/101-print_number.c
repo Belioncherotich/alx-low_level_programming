7e2e33e015125820013f8a3f3edcb02851d68b6e
Belioncherotich <velorinecodes@gmail.com>
2023-06-22 11:36:13 -0700

    Task on More functions, more nested loops

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,26 +1,35 @@
 #include "main.h"
 
 /**
- * print_number - print an integer
- * @n: integer to be printed
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-int _putchar(char c);
 void print_number(int n)
 {
-    if (n < 0)
-    {
-        _putchar('-');
-        n = -n;
-    }
-    else if (n == 0)
-    {
-        _putchar('0');
-        return;
-    }
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
-    if (n / 10)
-    {
-        print_number(n / 10);
-    }
-    _putchar((n % 10) + '0');
+	int divisor = 1;
+	int temp = n;
+
+	while (temp / 10 != 0)
+	{
+		divisor *= 10;
+		temp /= 10;
+	}
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
