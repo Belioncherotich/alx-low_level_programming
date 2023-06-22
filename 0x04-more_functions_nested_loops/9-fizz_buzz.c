9b536163062b00a7d938a628c42660a3d1ed868f
Belioncherotich <velorinecodes@gmail.com>
2023-06-22 11:20:40 -0700

    Task on More functions, more nested loops

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,8 +5,8 @@
  *
  * Description:
  * Prints the numbers from 1 to 100, but for multiples of three,
- * prints "Fizz" instead of the number, for multiples of five prints "Buzz",
- * and for numbers which are multiples of both three and five, prints "FizzBuzz".
+ * prints "Fizz" instead of the number, for multiples of five prints Buzz,
+ * and for numbers which are multiples of both three and five, prints FizzBuzz
  * Each number or word is separated by a space.
  *
  * Return: Always 0 (Success)
