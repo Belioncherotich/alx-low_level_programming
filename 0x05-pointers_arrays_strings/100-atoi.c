d08dd6c522ea8fc272014247c1413153b3b96cef
Belioncherotich <velorinecodes@gmail.com>
2023-06-26 05:09:54 -0700

    Task on Pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,39 +8,38 @@
  */
 int _atoi(char *s)
 {
-	int a, b, n, len, f, digit;
+        int a, b, len, f, digit;
 
-	a = 0;
-	b = 0;
-	c = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
+        a = 0;
+        b = 0;
+        len = 0;
+        f = 0;
+        digit = 0;
 
-	while (s[len] != '\0')
-		len++;
+        while (s[len] != '\0')
+                len++;
 
-	while (a < len && f == 0)
-	{
-		if (s[a] == '-')
-			++d;
+        while (a < len && f == 0)
+        {
+                if (s[a] == '-')
+                        ++a;
 
-		if (s[a] >= '0' && s[i] <= '9')
-		{
-			digit = s[a] - '0';
-			if (b % 2)
-				digit = -digit;
-			c = c * 10 + digit;
-			f = 1;
-			if (s[a + 1] < '0' || s[a + 1] > '9')
-				break;
-			f = 0;
-		}
-		a++;
-	}
+                if (s[a] >= '0' && s[a] <= '9')
+                {
+                        digit = s[a] - '0';
+                        if (b % 2)
+                                digit = -digit;
+                        a = a * 10 + digit;
+                        f = 1;
+                        if (s[a + 1] < '0' || s[a + 1] > '9')
+                                break;
+                        f = 0;
+                }
+                a++;
+        }
 
-	if (f == 0)
-		return (0);
+        if (f == 0)
+                return 0;
 
-	return (c);
+        return a;
 }
